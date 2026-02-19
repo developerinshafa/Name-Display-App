07ecbf03ff10cfd30f4c3a9a3f092ca31a266d90
developerinshafa <zuhrzaidha@gmail.com>
2026-02-19 14:13:54 +0530

    initing the name display app using react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='flex text-center justify-center bg-blue-400'>Hello Fathima Inshafa</h1>
+     
+    </>
+  )
+}
+
+export default App
