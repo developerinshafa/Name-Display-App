595f709ef8b47b0ea4080e2ad23298d2d801bdf6
developerinshafa <zuhrzaidha@gmail.com>
2026-02-20 13:10:58 +0530

    ui design change

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -4,17 +4,17 @@ export default function CartItem({ item }) {
     const [quantity, setQuantity] = useState(1);
 
     return (
-    <li className="grid justify-between items-center bg-blue-50 px-4 py-2 rounded w-250">
+    <li className="grid justify-between items-center bg-blue-50 border-b-8 border-l-8 border-blue-950 inset-shadow-sm inset-shadow-blue-700 shadow-xl shadow-blue-700/50 px-4 py-2 rounded w-250">
     <div className="grid items-center space-x-4">
         <img src={item.image} 
         alt={item.name} 
-        className="object-cover"
+        className="object-cover transition-transform duration-300 hover:scale-110"
         />
         <span className="text-xl p-2 font-semibold">{item.name}</span>
     </div>
     <div className="flex items-center space-x-14 bg-blue-200 text-black rounded font-semibold">
         <div className='space-x-2 p-2 '>
-            <button className=' hover:bg-red-600 rounded-full text-xl px-2' 
+            <button className=' hover:bg-red-500 rounded-full text-xl px-2' 
             onClick={() => setQuantity(() => Math.max(1,quantity - 1))} 
             >
             -</button>
